f4c12432913ba2300d9c979a964c16e02c1f70f3
ER1CK122 <erckgames53@gmail.com>
2025-01-27 20:31:24 -0300

    Implementado autenticação por middleware API Key. IssueId#06

---
+++ src/middleware/authenticationApiKey.ts
@@ -0,0 +1,29 @@
+import { Elysia } from 'elysia';
+import { supabase } from '../http/server';
+
+export const authenticationApiKey = new Elysia()
+  .onRequest(async ({ request, set }) => {
+    try {   
+      const apiKey = request.headers.get('x-api-key');
+      
+      if (!apiKey) {
+        set.status = 401;
+        return { error: 'API Key não fornecida' };
+      }
+
+      const { data, error } = await supabase
+        .from('api_keys')
+        .select('api_key')
+        .eq('api_key', apiKey)
+        .limit(1);
+      
+      if (error) throw error;
+      if (!data?.length) {
+        set.status = 401;
+        return { error: 'Chave da API não encontrada' };
+      }
+    } catch (error) {
+      set.status = 401;
+      return { error: "Erro ao autenticar a chave da API." };
+    }
+  });
